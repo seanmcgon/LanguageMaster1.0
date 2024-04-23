f019f368ff525776d9c7da62af46b7b9064c6add
mgkandeshwarath <mkandeshwara@umass.edu>
2024-04-23 01:33:37 -0400

    Edited assignments.js and assignmentTest.test.js so all tests pass

--- server/Testing/assignmentTest.test.js
+++ server/Testing/assignmentTest.test.js
@@ -1,19 +1,22 @@
 //cd into server and run using "npx jest Testing/assignmentTests"
 const { MongoClient } = require('mongodb');
-const { createAssignment, viewAssignment, addToAssignment, deleteAssignment, deleteFromAssignment } = require('../src/assignments.js');
+const { createAssignment, viewAssignment, addToAssignment, deleteAssignment, deleteFromAssignment, convertAssignmentToDtbForm } = require('../src/assignments.js');
 
 jest.mock('mongoose');
 const mongo = require('../src/assignments.js');
 
 describe('Assignment Management Tests', () => {
 
+      const connectionString = "mongodb+srv://mkandeshwara:0CgF5I8hwXaf88dy@cluster0.tefxjrp.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0&ssl=true";
+      const client = new MongoClient(connectionString);
+
       describe("addToAssignment", () => {
         it("Inserts flashcard to existing class and existing assignment", async () => {
-          const inserted = await mongo.addToAssignment("Spanish454_QRAPCC", "war", {text: "asdf", translation: "qwer", audio: "zcxv"});
+          const inserted = await addToAssignment("Spanish454_QRAPCC", "war", {text: "asdf", translation: "qwer", audio: "zcxv"});
           expect(inserted).toEqual(true);
           try{
-            await mongo.client.connect();
-            let db = mongo.client.db("Spanish454_QRAPCC");
+            await client.connect();
+            let db = client.db("Spanish454_QRAPCC");
             let col = db.collection("assignments");
             const card = await col.find({audio: "zcxv"}).toArray();
             expect(card.length).toEqual(1);
@@ -25,38 +28,38 @@ describe('Assignment Management Tests', () => {
             await col.deleteOne({audio: "zcxv"});
           }
           finally{
-            await mongo.client.close();
+            await client.close();
           }
         });
       
         it("Throws an error for a non-existent class", async () => {
           console.log = jest.fn();
-          const inserted = await mongo.addToAssignment("ABCD", "war", {text: "asdf", translation: "qwer", audio: "zcxv"});
+          const inserted = await addToAssignment("ABCD", "war", {text: "asdf", translation: "qwer", audio: "zcxv"});
           expect(inserted).toEqual(false);
           expect(console.log).toHaveBeenCalledWith("Class does not exist");
         });
       
         it("Throws an error for a non-existent assignment", async () => {
           console.log = jest.fn();
-          const inserted = await mongo.addToAssignment("Spanish454_QRAPCC", "ASDF", {text: "asdf", translation: "qwer", audio: "zcxv"});
+          const inserted = await addToAssignment("Spanish454_QRAPCC", "ASDF", {text: "asdf", translation: "qwer", audio: "zcxv"});
           expect(inserted).toEqual(false);
           expect(console.log).toHaveBeenCalledWith("Assignment does not exist");
         });
       })
       
-      describe('this suit will test the function delete Assignment',() =>{
-        it('check the presence of the assingment in the class first, then delete, and check the presence again. In this case, the assignments are already there', async() => {
-          const connectionString = "mongodb+srv://mkandeshwara:0CgF5I8hwXaf88dy@cluster0.tefxjrp.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0&ssl=true";
-          const client = new MongoClient(connectionString);
+      describe('deleteAssignment',() =>{
+        it('deletes an existing assignment', async() => {
           try{
           const class_Name = "Chinese671_JPYVGX";
+          const momentum = [{assignment: "momentum", card: 0, text: "mome", translation: "ent", audio: "https://www.adfaf.com"}, {assignment: "momentum", card: 1, text: "ent", translation: "um", audio: "https://www.adfaf.com"}]
           const assignment_name = "momentum";
           await client.connect();
           db = client.db(class_Name);
           col = await db.collection("assignments");
+          await col.insertMany(momentum);
           // Test the the presence of assignemnts in the given class
         const presence_of_assignments = (await col.find({assignment: assignment_name}).toArray());
-        expect(presence_of_assignments.length).toBeGreaterThan(0);
+        expect(presence_of_assignments.length).toBe(2);
         await deleteAssignment(class_Name, assignment_name);
        // Test the the presence of assignemnts in the given class after deleting
        await client.connect();
@@ -72,12 +75,11 @@ describe('Assignment Management Tests', () => {
             await client.close();
           }
         });
-        it('check the presence of the assingment in the class first, then delete, and check the presence again. In this case, the assignments are not there', async() => {
-          const connectionString = "mongodb+srv://mkandeshwara:0CgF5I8hwXaf88dy@cluster0.tefxjrp.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0&ssl=true";
-          const client = new MongoClient(connectionString);
+        it('the assignment to delete does not exist', async() => {
+
           try{
           const class_Name = "Chinese671_JPYVGX";
-          const assignment_name = "momentum";
+          const assignment_name = "adslkjf";
           await client.connect();
           db = client.db(class_Name);
           col = await db.collection("assignments");
@@ -100,26 +102,26 @@ describe('Assignment Management Tests', () => {
           }
         });
       });
-      describe('this suit will test the function delete object from a given Assignment',() =>{
-        it('delete only ones that match the object preference', async() => {
-          const connectionString = "mongodb+srv://mkandeshwara:0CgF5I8hwXaf88dy@cluster0.tefxjrp.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0&ssl=true";
-          const client = new MongoClient(connectionString);
+
+      describe('deleteFromAssignment',() =>{
+        it('deletes flashcards that match the object specified', async() => {
           try{
-          const class_Name = "Chinese671_JPYVGX";
-          const assignment_name = "bad";
-          await client.connect();
-          db = client.db(class_Name);
-          col = await db.collection("assignments");
-          // Test the the presence of assignemnts in the given class
-        const presence_of_assignments = (await col.find({assignment: assignment_name}).toArray());
-        expect(presence_of_assignments.length).toEqual(3);
-        await deleteFromAssignment(class_Name, assignment_name,{text: "international", translation: "low",audio: "http://www.webster-durham.info/"});
-       // Test the the presence of assignemnts in the given class after deleting
-       await client.connect();
-       db1 = client.db(class_Name);
-       col1 = await db1.collection("assignments");
-        const presence_after = await col1.find({assignment: assignment_name}).toArray();
-        expect(presence_after.length).toEqual(2);
+            const class_Name = "Chinese671_JPYVGX";
+            const assignment_name = "bad";
+            await client.connect();
+            db = client.db(class_Name);
+            col = await db.collection("assignments");
+            // Test the the presence of assignemnts in the given class
+            col.insertOne({assignment: assignment_name, card: 2, text: "national", translation: "low", audio: "http://www.webster-durham.info/"})
+            const presence_of_assignments = (await col.find({assignment: assignment_name}).toArray());
+            expect(presence_of_assignments.length).toEqual(3);
+            await deleteFromAssignment(class_Name, assignment_name,{text: "national", translation: "low", audio: "http://www.webster-durham.info/"});
+          // Test the the presence of assignemnts in the given class after deleting
+            await client.connect();
+            db1 = client.db(class_Name);
+            col1 = await db1.collection("assignments");
+            const presence_after = await col1.find({assignment: assignment_name}).toArray();
+            expect(presence_after.length).toEqual(2);
           }
           catch(error){
             throw (error);
@@ -129,6 +131,7 @@ describe('Assignment Management Tests', () => {
           }
         });
       });
+
       describe('convertAssignmentToDtbForm', () => {
         it ('should convert an assignment array to the correct format', () => {
           const assignmentName = 'Assignment0';
@@ -137,7 +140,7 @@ describe('Assignment Management Tests', () => {
             {text: 'text1', translation: 'translation1', audio: 'audio1'},
             {text: 'text2', translation: 'translation2', audio: 'audio2'}
           ];
-          const convertedAssignment = mongo.convertAssignmentToDtbForm(assignmentName, assignmentArray);
+          const convertedAssignment = convertAssignmentToDtbForm(assignmentName, assignmentArray);
           expect(convertedAssignment).toEqual([
             {assignment: 'Assignment0', card: 0, text: 'text0', translation: 'translation0', audio: 'audio0'},
             {assignment: 'Assignment0', card: 1, text: 'text1', translation: 'translation1', audio: 'audio1'},
@@ -149,22 +152,23 @@ describe('Assignment Management Tests', () => {
       describe("viewAssignment", () => {
         // Works in a typical case, where no errors should be thrown and a valid output should be returned
         it("Returns existing assignment in correct form", async () => {
-          const cards = await mongo.viewAssignment("Spanish454_QRAPCC", "war");
-          expect(cards.length).toEqual(5);
-          expect(cards.map(e => e.text)).toEqual(["right", "provide"]);
+          const cards = await viewAssignment("Spanish454_QRAPCC", "war");
+          expect(cards.length).toEqual(2);
+          expect(cards.map(e => e.text)).toContain("right");
+          expect(cards.map(e => e.text)).toContain("provide");
         });
       
         // Reacts correctly for class that doesn't exist
         it("Throws an error for a non-existent class", async () => {
           console.log = jest.fn();
-          await mongo.viewAssignment("ABCD", "war");
+          await viewAssignment("ABCD", "war");
           expect(console.log).toHaveBeenCalledWith("Class does not exist");
         });
       
         // Reacts correctly for assignment that doesn't exist
         it("Throws an error for a non-existent assignment", async () => {
           console.log = jest.fn();
-          await mongo.viewAssignment("Spanish454_QRAPCC", "ABCD");
+          await viewAssignment("Spanish454_QRAPCC", "ABCD");
           expect(console.log).toHaveBeenCalledWith("Assignment does not exist");
         });
       });
