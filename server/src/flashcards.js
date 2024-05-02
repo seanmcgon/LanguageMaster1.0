9221efdf17144c66a5b3004a3ab9fa1f606b2f44
Jason Huang <jyhuang@umass.edu>
2024-05-02 14:00:06 -0400

    flashcard stop recording calls backend

--- server/src/flashcards.js
+++ server/src/flashcards.js
@@ -1,8 +1,17 @@
 // This was giving me issues (Error: Cannot find module './auth/googleauth')
-// const {audioRecognition} = require("./transcription/audioToText.js");
+const {audioRecognition} = require("./transcription/audioToText.js");
 
 async function getFeedback(curWord, audioFile) {
-    // Dummy function for now
+   // Example audio file
+    const audioUrl = 'https://www2.cs.uic.edu/~i101/SoundFiles/preamble10.wav';
+    audioRecognition(audioUrl)
+    .then(results => {
+        console.log('Transcription:', results);
+    })
+    .catch(error => {
+        console.error('Error:', error);
+    });
+
     return {};
 }
 
