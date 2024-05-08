72cafef3d9e3737bce3a9d318d08e73ba8e05102
Sean McGonigle <smcgonigle@umass.edu>
2024-05-08 11:47:07 -0400

    Prep 2

--- client/src/components/socket.js
+++ client/src/components/socket.js
@@ -1,6 +1,6 @@
 import { io } from 'socket.io-client';
 
-const socket = io('http://localhost:3001');
+const socket = io('https://languagemaster1-0.onrender.com');
 
 export const connectSocket = () => {
   socket.on("connect", () => {
